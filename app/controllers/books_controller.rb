1458a86f55538664080a6c9d589ec71405ba62ea
yamauchimiku <yamamochi7171@gmail.com>
2021-09-19 14:31:45 +0000

    [Start] 4-10

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,35 @@
+class BooksController < ApplicationController
+  
+
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    @book.save
+    redirect_to book_path(@book.id)
+  end
+
+  def index
+    @books = Book.all
+    @book = Book.new
+  end
+
+  def show
+  end
+
+  def edit
+
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+  def book_params
+    params.require(:book).permit(:title,:body)
+
+  end
+
+end
