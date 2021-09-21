6d0a937db8c76cb0dab56aee4660b50421cc57b5
yamauchimiku <yamamochi7171@gmail.com>
2021-09-21 13:29:53 +0000

    [Update] 4-10

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,10 +2,13 @@ class UsersController < ApplicationController
 
   def index
     @users = User.all
+    @user = current_user
+    @book = Book.new
   end
   def show
     @user = User.find(params[:id])
     @books = @user.books
+    @book = Book.new
   end
   def edit
     @user = User.find(params[:id])
@@ -20,6 +23,6 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:name, :profile_image)
+    params.require(:user).permit(:name, :profile_image, :introduction)
   end
 end
