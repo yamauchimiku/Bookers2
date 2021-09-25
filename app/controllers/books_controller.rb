d5bb9261eb9427914f9b87812cb6208479b5549b
yamauchimiku <yamamochi7171@gmail.com>
2021-09-25 09:35:07 +0000

    [Finish] 4-10

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,8 +4,14 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
-    @book.save
-    redirect_to book_path(@book.id)
+    if @book.save
+      flash[:notice] =  'You have created book successfully.'
+      redirect_to book_path(@book.id)
+    else
+      @user = current_user
+      @books = Book.all
+       render :index
+    end
   end
 
   def index
@@ -16,17 +22,27 @@ class BooksController < ApplicationController
 
   def show
     @book = Book.find(params[:id])
-    @user = current_user
+    @user = @book.user
+    @newbook = Book.new
   end
 
   def edit
     @book = Book.find(params[:id])
+    if @book.user == current_user
+      render "edit"
+    else
+      redirect_to books_path
+    end
   end
 
   def update
     @book = Book.find(params[:id])
-    @book.update(book_params)
-     redirect_to book_path(@book)
+    if @book.update(book_params)
+      flash[:notice] = 'You have updated book successfully.'
+      redirect_to book_path(@book)
+    else
+      render :edit
+    end
   end
 
   def destroy
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
-  get 'users/show'
   root to: 'homes#top'
-  get 'about' => 'homes#about'
+  get 'home/about' => 'homes#about'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :books
-  resources :users, only: [:show, :edit, :update, :index]
+  resources :users
 end
